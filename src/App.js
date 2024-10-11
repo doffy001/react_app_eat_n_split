bb116300723e32fb34e77b4ab536eb085acda701
FASTCODING Doffy <doffy.fastcodingvn@gmail.com>
2024-10-11 17:48:35 +0700

    [upd] toggle form split bill

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Children, useState } from 'react';
+import { useState } from 'react';
 
 const initialFriends = [
   {
@@ -32,47 +32,67 @@ function Button({ children, onClick }) {
 function App() {
   const [friends, setFriends] = useState(initialFriends);
   const [isOpenFormAddFriend, setIsOpenFormAddFriend] = useState(false);
+  const [formSplitBillId, setFormSplitBillId] = useState(null);
+  const currentFriendName = friends.find((friend) => friend.id === formSplitBillId)?.name;
+
+  function handleToggleFormSplitBill(id) {
+    setFormSplitBillId((currentId) => (currentId === id ? null : id));
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <Friends friends={friends} />
+        <Friends
+          friends={friends}
+          formSplitBillId={formSplitBillId}
+          onToggleFormSplitBill={handleToggleFormSplitBill}
+        />
         {isOpenFormAddFriend && <FormAddFriend />}
         <Button onClick={() => setIsOpenFormAddFriend(!isOpenFormAddFriend)}>
           {isOpenFormAddFriend ? 'Close' : 'Add friend'}
         </Button>
       </div>
-      <FormSplitBill />
+      {formSplitBillId && <FormSplitBill currentFriendName={currentFriendName} />}
     </div>
   );
 }
 
-function Friends({ friends }) {
+function Friends({ friends, formSplitBillId, onToggleFormSplitBill }) {
   return (
     <ul>
       {friends.map((friend) => {
-        return <Friend friend={friend} />;
+        return (
+          <Friend
+            friend={friend}
+            key={friend.id}
+            formSplitBillId={formSplitBillId}
+            onToggleFormSplitBill={onToggleFormSplitBill}
+          />
+        );
       })}
     </ul>
   );
 }
 
-function Friend({ friend }) {
+function Friend({ friend, formSplitBillId, onToggleFormSplitBill }) {
   return (
     <li className="selected">
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
       {friend.balance < 0 && (
-        <p class="red">
+        <p className="red">
           You own {friend.name} ${Math.abs(friend.balance)}
         </p>
       )}
       {friend.balance > 0 && (
-        <p class="green">
+        <p className="green">
           {friend.name} owns you ${friend.balance}
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <Button>Select</Button>
+      <Button onClick={() => onToggleFormSplitBill(friend.id)}>
+        {formSplitBillId === friend.id ? 'Close' : 'Select'}
+      </Button>
     </li>
   );
 }
@@ -89,7 +109,7 @@ function FormAddFriend() {
   );
 }
 
-function FormSplitBill() {
+function FormSplitBill({ currentFriendName }) {
   return (
     <form className="form-split-bill">
       <h2>Split the bill with ?</h2>
@@ -97,12 +117,12 @@ function FormSplitBill() {
       <input type="number" />
       <label>🧍‍♂️ Your expense</label>
       <input type="number" />
-      <label>👭 ?'s expense</label>
+      <label>👭 {currentFriendName}'s expense</label>
       <input type="number" disabled />
       <label>👭 Who is paying the bill?</label>
       <select>
         <option>You</option>
-        <option>?</option>
+        <option>{currentFriendName}</option>
       </select>
       <Button>Split bill</Button>
     </form>
